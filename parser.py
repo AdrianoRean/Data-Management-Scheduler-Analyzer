fc6b7eb2715b320e32fc1d099792416e48301216
AdrianoRean <127209495+AdrianoRean@users.noreply.github.com>
2024-07-31 13:40:08 +0200

    Parsing and creating complete

--- parser.py
+++ parser.py
@@ -3,31 +3,97 @@ import json
 
 from schedules import schedules
 
-schedule_to_analyze = 1
+schedule_to_analyze = 0
 
 blind_write = False
+info = (0, [])
 elements_touched_transactions = {}
+'''
+Nella forma di  (per due transazioni e due elementi):
+{
+    "element_1" : [
+        (transaction_0, "W")
+    ],
+    "element_2" : [
+        (transaction_1, "R"), (transaction_1, "W"), (transaction_0, "R")
+    ]
+}
+'''
 graph = []
+'''
+Nella forma di (per due transazioni e due elementi):
+[
+    [False, False],
+    [True, False]
+]
+'''
 read_from = {}
+'''
+Nella forma di (per due transazioni e due elementi):
+{
+    "transaction_0" : {
+        "element_1" : "",
+        "element_2" : trasaction_2
+    },
+    "transaction_1" : {
+        "element_1" : "",
+        "element_2" : ""
+    }
+}
+'''
 final_write = {}
+'''
+Nella forma di  (per due transazioni e due elementi):
+{
+    "element_1" : transaction_0,
+    "element_2" : transaction_1
+}
+'''
 
 to_serial = {}
+'''
+Nella forma di (per due transazioni e due elementi):
+{
+    "transaction_0" : [
+        ["R", "transaction_0", "element_2"}, ["W", "transaction_0", "element_1"}
+    ],
+    "transaction_1" : [
+        ["R", "transaction_1", "element_2"}, ["W", "transaction_1", "element_2"}
+    ]
+}
+'''
 serial_schedules = {}
+'''
+Nella forma di (per due transazioni e due elementi):
+{
+    "01" : [
+        ["R", "transaction_0", "element_2"], 
+        ["W", "transaction_0", "element_1"], 
+        ["R", "transaction_1", "element_2"], 
+        ["W", "transaction_1", "element_2"]
+    ],
+    "10" : [
+        ["R", "transaction_1", "element_2"], 
+        ["W", "transaction_1", "element_2"], 
+        ["R", "transaction_0", "element_2"],
+        ["W", "transaction_0", "element_1"]
+    ]
+}
+'''
 
-def parse(or_schedule):
+def parse(or_schedule, n_transactions, elements):
     
     #Initialization
     schedule = deepcopy(or_schedule)
-    info = schedule.pop(0)
-    for i in range(0, info[0]):
-        graph.append([False for j in range (0, info[0])])
+    for i in range(0, n_transactions):
+        graph.append([False for j in range (0, n_transactions)])
         read_from[str(i)] = {}
         to_serial[str(i)] = []
-    for element in info[1]:
+    for element in elements:
         elements_touched_transactions[element] = set([])
         final_write[element] = None
-        for i in range(0, info[0]):
-            read_from[str(i)][element] = (None, None)
+        for i in range(0, n_transactions):
+            read_from[str(i)][element] = None
         
     
     #Actual Parsing
@@ -48,19 +114,75 @@ def parse(or_schedule):
                     
             elements_touched_transactions[element].add((transaction, element))
             if type_action == "W":
-                if read_from[transaction][element] == (None,None):
+                if read_from[transaction][element] == "":
                     blind_write = True
                 final_write[element] = transaction
             else:
-                read_from[transaction][element] == (transaction, final_write[element])
+                read_from[transaction][element] = final_write[element]
                 
-        to_serial[transaction].append((type_action, element))
-        
-def generate_serial(start, transactions):
-    for i in range (start):
-        pass
+        to_serial[transaction].append(action)
 
-def parse_serial(schedule):
-    pass
+def parse_serial(schedule_name, n_transactions, elements):
+    #Initialization
+    schedule = deepcopy(serial_schedules[schedule_name]["schedule"])
+    serial_read_from = serial_schedules[schedule_name]["read_from"]
+    serial_final_write = serial_schedules[schedule_name]["final_write"]
+    
+    for i in range(0, n_transactions):
+        serial_read_from[str(i)] = {}
+    for element in elements:
+        serial_final_write[element] = None
+        for i in range(0, n_transactions):
+            serial_read_from[str(i)][element] = None
         
-parse(schedules[0])
+    #Actual Parsing
+    for action in schedule:
+        
+        type_action = action[0]
+        transaction = action[1]
+        element = action[2]
+        
+        if element != "":
+            if type_action == "W":
+                serial_final_write[element] = transaction
+            else:
+                serial_read_from[transaction][element] = serial_final_write[element]
+        
+def generate_serial(remaining, order, n_transactions, elements):
+    if remaining == []:
+        #crea schedule
+        new_serial = []
+        for transaction in order:
+            new_serial += to_serial[transaction]
+        name = ''.join(order)
+        serial_schedules[name] = {}
+        serial_schedules[name]["schedule"] = new_serial
+        serial_schedules[name]["read_from"] = {}
+        serial_schedules[name]["final_write"] = {}
+        parse_serial(name, n_transactions, elements)
+    else:
+        for transaction in remaining:
+            new_remaining = deepcopy(remaining)
+            new_remaining.remove(transaction)
+            new_order = deepcopy(order)
+            new_order.append(transaction)
+            generate_serial(new_remaining, new_order, n_transactions, elements)
+                
+def parse_and_create(schedule):
+    info = schedule.pop(0)
+    parse(schedule, info[0], info[1])
+    generate_serial([str(transaction) for transaction in range (0, info[0])],[], info[0], info[1])
+    
+    with open(str(schedule_to_analyze) + '.json', 'w') as f:
+        data = {
+                "blind_write" : blind_write,
+                "graph" : graph,
+                "read_from" : read_from,
+                "final_write" : final_write,
+        }
+        json.dump(data, f, indent=4)
+        
+    with open(str(schedule_to_analyze) + '_serials.json', 'w') as f:
+        json.dump(serial_schedules, f, indent=4)
+    
+parse_and_create(schedules[schedule_to_analyze])
