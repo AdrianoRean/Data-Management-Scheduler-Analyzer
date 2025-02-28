ba753996ae20ac4328f8a2d7c818fe6a3aaa3da5
AdrianoRean <127209495+AdrianoRean@users.noreply.github.com>
2025-02-28 15:23:55 +0100

    Fixato view

--- sequential_checker.py
+++ sequential_checker.py
@@ -43,6 +43,7 @@ def sequential_checker(schedule):
 
 if __name__ == "__main__":
     time_results = {}
+    #schedules = schedules[-3:-2]
     for s in schedules:
         start_time = time.perf_counter() # Avvia il timer
         result = sequential_checker(s)
