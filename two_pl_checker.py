291faff6ef27fa4ef5a44e69777cae0dcbbfa0c4
AdrianoRean <127209495+AdrianoRean@users.noreply.github.com>
2025-03-05 12:39:58 +0100

    Update two_pl_checker.py

--- two_pl_checker.py
+++ two_pl_checker.py
@@ -25,17 +25,12 @@ class TwoPLChecker:
                 transaction.append(t)
         return False
         
-    def check_if_lock_available(self, transaction, action, resource, index, loop=[]):
+    def check_if_lock_available(self, transaction, action, resource, index, loop_resources=[], loop_transactions=[]):
         ''' 𝑂(R⋅T)'''
         # Sono il primo?
         if index != 0:
             previous_transaction, previous_action = self.transactions_involved[resource][index - 1]
-
-        # 𝑂(min(R,T))
-        if self.need_same(loop):
-            return False
-        
-        if index== 0:
+        else:
             return True
         
         # Shared lock?
@@ -49,11 +44,13 @@ class TwoPLChecker:
             
             for (other_resource,other_action) in self.resources_needed[str(previous_transaction)]:
                 other_index = self.transactions_involved[other_resource].index((previous_transaction,other_action))
-                if (previous_transaction,other_resource) in loop:
+                
+                if other_resource in loop_resources or previous_transaction in loop_transactions:
                     return False
-                else:
-                    loop.append((previous_transaction,other_resource))
-                result = self.check_if_lock_available(previous_transaction, other_action, other_resource, other_index,loop.copy())
+                loop_resources.append(other_resource)
+                loop_transactions.append(previous_transaction)
+                    
+                result = self.check_if_lock_available(previous_transaction, other_action, other_resource, other_index, loop_resources.copy(), loop_transactions.copy())
                 if not result:
                     return False
             return True
@@ -91,7 +88,7 @@ class TwoPLChecker:
             #                               risorsa da lockare
             index = self.transactions_involved[resource].index((tr,action))
             # per ogni transazione che richiede la risorsa prima di lui controlla se ha qualcosa
-            result = self.check_if_lock_available(tr, action, resource, index,loop=[(tr,resource)])
+            result = self.check_if_lock_available(tr, action, resource, index,loop_resources=[resource], loop_transactions=[tr])
             if not result:
                 return False
             #print(f"Tr: {str(tr)}, Action: {action}, Resource: {resource}, R_Needed: {resources_needed[str(tr)]}")
