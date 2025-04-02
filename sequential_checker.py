105f21ad17c8b71a0b305ba0503b3552c242296a
AdrianoRean <127209495+AdrianoRean@users.noreply.github.com>
2025-04-02 15:41:45 +0200

    Update sequential_checker.py

--- sequential_checker.py
+++ sequential_checker.py
@@ -40,7 +40,7 @@ def sequential_checker(schedule):
 if __name__ == "__main__":
     time_results = {}
     #schedules = schedules[-3:-2]
-    for s in view_schedules:
+    for s in schedules:
         start_time = time.perf_counter() # Avvia il timer
         result = sequential_checker(s)
         end_time = time.perf_counter()  # Ferma il timer 
